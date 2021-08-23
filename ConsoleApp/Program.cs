3a79ffcd49314927cc0bd2f42eaea04eeafced25
acchmedha <ahmedha@maestralsolutions.com>
2021-08-23 12:31:23 +0200

    This is changing line

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Next Line");
+            Console.WriteLine("Another Line");
         }
     }
 }
